bc6aac8fbf59282f64cc17fb136018171639bbda
kosh-b <roeebaba@gmail.com>
2022-09-19 00:07:59 +0300

    Update league table on workflow (#4)
    * Fetch maccabi games from season 22-23.
    Add support for  uploading newspapers automatically by finding the game page (taken from the paper file name)
    
    * Update league table every day

---
+++ update_league_table.py
@@ -0,0 +1,36 @@
+import logging
+
+import mwparserfromhell
+import requests
+
+from pywikibot_boilerplate import run_boilerplate
+
+_LATEST_LEAGUE_TABLE_STATUS_URL = 'https://rona.sh/api/maccabipedia'
+_LEAGUE_TABLE_TEMPLATE_ON_MACCABIPEDIA = 'תבנית:טבלת ליגה 2022/23'
+_TABLE_STATUS_KEY = 'נתוני טבלה'
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
+# We need to log before we run any of our maccabipedia (pywikibot or it's import) related code
+site = run_boilerplate()
+
+import pywikibot as pw
+
+
+def update_league_table_status() -> None:
+    logging.info(f'Fetching current league table from: {_LATEST_LEAGUE_TABLE_STATUS_URL}')
+    league_table_status = requests.get(_LATEST_LEAGUE_TABLE_STATUS_URL)
+
+    league_table_template_page = pw.Page(site, _LEAGUE_TABLE_TEMPLATE_ON_MACCABIPEDIA)
+
+    parsed_mw_text = mwparserfromhell.parse(league_table_template_page.text)
+    table_template = parsed_mw_text.filter_templates(_LEAGUE_TABLE_TEMPLATE_ON_MACCABIPEDIA)[0]
+    table_template.add(_TABLE_STATUS_KEY, league_table_status)
+
+    league_table_template_page.text = parsed_mw_text
+
+    league_table_template_page.save(summary="MaccabiBot - Update league table")
+
+
+if __name__ == '__main__':
+    update_league_table_status()

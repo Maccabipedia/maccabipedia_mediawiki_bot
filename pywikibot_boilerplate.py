8a5ec2a3fd1c31dc47fca9fe362975a48638f1e9
kosh-b <roeebaba@gmail.com>
2022-09-19 00:07:05 +0300

    Update league table every day

--- pywikibot_boilerplate.py
+++ pywikibot_boilerplate.py
@@ -7,10 +7,12 @@ from pywikibot import config
 MACCABIPEDIA_FAMILY_FILE = Path(__file__).absolute().parent / 'pywikibot_configs' / 'maccabipedia_family.py'
 
 
-def run_boilerplate() -> None:
+def run_boilerplate() -> pw.Site:
     logging.info('Running pywikibot boilerplate code')
     config.family_files['maccabipedia'] = str(MACCABIPEDIA_FAMILY_FILE)
 
     # We need to have the url to api.php due to the fact we don't have w/api.php
-    s = pw.Site()
-    s.login()
+    site = pw.Site()
+    site.login()
+
+    return site

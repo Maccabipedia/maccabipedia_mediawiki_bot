7d83f7d10d268dcf95a12377d7b73c333f2ba5e5
kosh-b <roeebaba@gmail.com>
2022-11-05 10:53:33 +0200

    Monkeypatch to workaround the loginreturnurl of pywikibot

--- pywikibot_boilerplate.py
+++ pywikibot_boilerplate.py
@@ -1,18 +1,35 @@
 import logging
 from pathlib import Path
+from typing import Dict
 
 import pywikibot as pw
 from pywikibot import config
 
 MACCABIPEDIA_FAMILY_FILE = Path(__file__).absolute().parent / 'pywikibot_configs' / 'maccabipedia_family.py'
 
+# We can't send the get param 'loginreturnurl' with example.com, and it's hardcoded, so have to replace it
+from pywikibot.data.api import LoginManager
+
+old_pywikibot_login = LoginManager._login_parameters
+
+
+def new_pywikibot_login(*args, **kwargs) -> Dict[str, str]:
+    old_params = old_pywikibot_login(*args, **kwargs)
+
+    old_params['loginreturnurl'] = 'https://maccabipedia.co.il'
+
+    return old_params
+
 
 def run_boilerplate() -> pw.Site:
     logging.info('Running pywikibot boilerplate code')
     config.family_files['maccabipedia'] = str(MACCABIPEDIA_FAMILY_FILE)
 
+    LoginManager._login_parameters = new_pywikibot_login
+
     # We need to have the url to api.php due to the fact we don't have w/api.php
     site = pw.Site()
+    site.logout()
     site.login()
 
     return site
